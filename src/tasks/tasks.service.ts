633c16c7a58c29b2b8300bcbd39be211426efd9c
ramtin <ramtinsharif@gmail.com>
2022-03-02 13:01:53 +0330

    add basic create task to service and controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { ITask } from './task.model';
-
+import { ITask, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
-  private tasks:ITask[] = [];
+  private tasks: ITask[] = [];
 
-  getAllTasks():ITask[] {
+  getAllTasks(): ITask[] {
     return this.tasks;
+  }
+
+  createTasks(title: string, description: string): ITask {
+    const task: ITask = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
