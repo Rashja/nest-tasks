f46a89ede6b181cde8423604646368c7fa64ded1
ramtin <ramtinsharif@gmail.com>
2022-03-02 12:27:11 +0330

    add task model type

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
   
   @Get()
-  getAllTaks(){
+  getAllTaks():ITask[]{
     return this.taskService.getAllTasks()
   }
 }
