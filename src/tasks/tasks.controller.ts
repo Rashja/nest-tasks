db30cc7005790cd89e1010d2e1d0965f6849c760
ramtin <ramtinsharif@gmail.com>
2022-03-03 10:53:03 +0330

    add filters tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,18 +6,24 @@ import {
   Param,
   Delete,
   Patch,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { ITask, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTaks(): ITask[] {
-    return this.taskService.getAllTasks();
+  getTasks(@Query() filterDto:GetTasksFilterDto): ITask[] {
+    if(Object.keys(filterDto).length){
+      return this.taskService.getTasksByFilters(filterDto)
+    }else {
+      return this.taskService.getAllTasks();
+    }
   }
 
   @Get('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Get, Injectable } from '@nestjs/common';
 import { ITask, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -20,10 +21,29 @@ export class TasksService {
     return this.tasks;
   }
 
-  updateTaskStatus(id:string,status:TaskStatus):ITask{
-    const task=this.getTaskById(id);
-    task.status=status;
+  updateTaskStatus(id: string, status: TaskStatus): ITask {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
+  }
+
+  getTasksByFilters(filterDto: GetTasksFilterDto): ITask[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = this.tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+    }
+    return tasks;
   }
 
   createTasks(createTaskDto: CreateTaskDto): ITask {
